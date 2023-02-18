eac8d609e134947b2b9ad656cf81b4b6d98deff8
Ema <ema.jamakovic@stu.ibu.edu.ba>
2023-02-18 16:10:38 +0100

    Beauty tips firebase update

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import BeautyTips from "./components/BeautyTips";
-import BeautyTipsInfo from "./components/BeautyTipsInfo";
 import Reviews from "./components/Reviews";
 import Bestseller from "./components/Bestseller";
 import CategoryMP from "./components/CategoryMP";
@@ -15,7 +14,10 @@ import { query, orderBy, limit } from "firebase/firestore";
 
 export default function Home() {
     const[reviews, setReviews]=React.useState([]);
+    const[beautytips, setbeautyTips]=React.useState([]);
+
     const reviewCollectionRef=collection(db, "reviews");
+    const beautytipsCollectionRef=collection(db, "beautytips");
 
     useEffect( () => {
         const getReviews= async () => {
@@ -23,12 +25,20 @@ export default function Home() {
             const data=await getDocs(q);  
             setReviews(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
         };
+
+        const getBeautyTips= async()=> {
+            const q=query(beautytipsCollectionRef, limit(3))
+            const data=await getDocs(q);
+            setbeautyTips(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        };
+
         getReviews();
+        getBeautyTips();
     }, []);
 
 
 
-const beautytips=BeautyTipsInfo.map(beautytip => {
+const beautytipscomponent=beautytips.map(beautytip => {
     return (
     <BeautyTips 
         key={beautytip.id}
@@ -61,7 +71,7 @@ return(
 <MainPhoto/>
 <div className="clasifier">BEAUTY TIPS</div>
 <div className="beautytips">
-    {beautytips}
+    {beautytipscomponent}
 </div>
 
 <div className="categories">{categories}</div>
--- src/components/BeautyTips.js
+++ src/components/BeautyTips.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function BeautyTips(props) {
     return (
         <div className="beautytip">
+            <a href={props.url}>
             <img className="beauty_image" src={props.img}/>
             <h2 className="beauty_title">{props.title}</h2>
+            </a>
         </div>
     )
 }
